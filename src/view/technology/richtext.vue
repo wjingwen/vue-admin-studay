c7ca82be97291abe0fc7b95f503f310881330f9c
wangjingwen <597906933@qq.com>
2019-12-20 08:26:26 +0800

    编辑器

--- src/view/technology/richtext.vue
+++ src/view/technology/richtext.vue
@@ -1,8 +1,29 @@
 <template>
+  <div class="edit_container">
+        <quill-editor
+            v-model="content"
+            ref="myQuillEditor"
+            :options="editorOption">
+        </quill-editor>
+        <button>保存</button>
+    </div>
 </template>
 
 <script>
+  import Vue from 'vue'
+  import VueQuillEditor from 'vue-quill-editor'
+  import 'quill/dist/quill.core.css'
+  import 'quill/dist/quill.snow.css'
+  import 'quill/dist/quill.bubble.css'
+  Vue.use(VueQuillEditor)
+  export default{
+    data(){
+      return{
+        editorOption:{},
+        content:''
+      }
+    }
+  }
 </script>
-
-<style>
+<style scoped>
 </style>
